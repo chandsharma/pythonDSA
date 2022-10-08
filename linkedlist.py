77a86a3edf2fa3fa93218909feffc55a6cb3e6bd
Chandan Sharma <chandanlalka@gmail.com>
2022-10-08 15:01:15 +0530

    all functions done

--- linkedlist.py
+++ linkedlist.py
@@ -50,10 +50,46 @@ class LinkedList():
     
     def remove_from_end(self):
         node = self.head
-        if self.size <= 1:
+        if self.size() <= 1:
             self.remove_from_beginning()
             return
-        
+        while node.next.next:
+            node = node.next
+        node.next = None
+    
+    def inser_in_batch(self,data_list):
+        for data in data_list[::-1]:
+            self.insert_at_beginning(data)
+
+    def insert_at_index(self,index,data):
+        size = self.size()
+        if index<0 or index>=size:
+            raise Exception('Index out of bound')
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+        itr = 0
+        current_node = self.head
+        while not itr == index-1: 
+            current_node = current_node.next
+            itr += 1
+        node = Node(data)
+        node.next = current_node.next
+        current_node.next = node
+
+    def remove_from_index(self,index):
+        size = self.size()
+        if index<0 or index>=size or self.head is None:
+            raise Exception('Invalid Index')
+        node = self.head
+        if index == 0:
+            self.remove_from_beginning()
+            return
+        itr = 0 
+        while not itr == index-1:
+            node = node.next
+            itr += 1
+        node.next = node.next.next
 
 if __name__ == '__main__':
     ll = LinkedList()
@@ -62,7 +98,12 @@ if __name__ == '__main__':
     ll.insert_at_beginning(5)
     ll.insert_at_end(334)
     ll.print()
+    ll.inser_in_batch([12,334,456,324,213,56,67,])
+    ll.print()
     print(ll.size())
-    ll.remove_from_beginning()
-    print(ll.size())
+    # ll.remove_from_beginning()
+    # ll.insert_at_index(10,99)
+    # print(ll.size())
+    # ll.remove_from_end()
+    ll.remove_from_index(5)
     ll.print()

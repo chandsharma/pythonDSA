cfdaff87c0cc55add51c6e39fa69f15a48164359
Chandan Sharma <chandanlalka@gmail.com>
2022-10-09 14:53:34 +0530

    1st and 2nd exercise done

--- HashMaps/Exercise/1.py
+++ HashMaps/Exercise/1.py
@@ -4,14 +4,26 @@ sys.path.append(__file__.split('/Exercise')[0])  #added path of parent directory
 from hashMaps_collisionHandling import HashMap
 
 def load_data_as_dict(container):
-    with open("nyc_weather.csv",'r') as file:
-        csv_contents = file.readlines[1:]
+    with open('/home/chand/Desktop/pythonDSA/HashMaps/Exercise/nyc_weather.csv','r') as file:
+        csv_contents = file.readlines()[1:]
         for content in csv_contents:
-            container[content.split(',')[0]] = content.split(',')[1]
+            container[content.split(',')[0]] = int(content.split(',')[1])
+
+def load_data_as_list():
+    with open('/home/chand/Desktop/pythonDSA/HashMaps/Exercise/nyc_weather.csv','r') as file:
+        csv_contents = file.readlines()[1:]
+        data = []
+        for content in csv_contents:
+            data.append(int(content.split(',')[1]))
+        return data
 
 if __name__ == '__main__':
-    f = open("nyc_weather.csv",'r')
     hm = HashMap()
     load_data_as_dict(hm)
-    print(hm.arr)
+    # data = load_data_as_list()
+    # print(data)
+    # print(sum(data[:7])/len(data[:7]))
+    # print(max(data[:10]))
+    print(hm['Jan 9'])
+    print(hm['Jan 4'])
 

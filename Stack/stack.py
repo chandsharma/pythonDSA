c1682be2c1168cea8caaa72aaa26b667f7609f21
Chandan Sharma <chandanlalka@gmail.com>
2022-10-13 10:27:53 +0530

    stack implementation using list()

--- Stack/stack.py
+++ Stack/stack.py
@@ -1,1 +1,28 @@
 #stack implementation in python
+#List can be also used as Stack in python
+
+stack_data = []
+
+stack_data.append('first')
+stack_data.append('second')
+stack_data.append('third')
+stack_data.append('fourth')
+stack_data.append('last')
+
+#now the data in list is stored as
+print(stack_data)
+
+#stack is <Last in First Out>
+#which can be acheived by pop as
+print(stack_data.pop())
+print(stack_data)
+print(stack_data.pop())
+print(stack_data)
+print(stack_data.pop())
+print(stack_data)
+print(stack_data.pop())
+print(stack_data)
+print(stack_data.pop())
+print(stack_data)
+print(stack_data.pop())
+print(stack_data)
